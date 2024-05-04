20d9610e4d9649aa87180e726a47808a1843b01c
Luis Parrado <luisprmat@gmail.com>
2024-05-04 18:54:31 -0500

    Routes with parameters, restrict with regexp

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,14 @@ Route::get('/notas', function () {
     return 'Listado de notas';
 });
 
+Route::get('/notas/{id}', function ($id) {
+    return 'Detalle de la nota: '.$id;
+});
+
 Route::get('/notas/crear', function () {
     return 'Crear nueva nota';
+});
+
+Route::get('/notas/{id}/editar', function ($id) {
+    return 'Editar nota: '.$id;
 });
