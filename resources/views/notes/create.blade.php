5d14c4f1ce91783c190cabe7d31815b44a8a7a10
Luis Parrado <luisprmat@gmail.com>
2024-05-04 22:28:20 -0500

    Revert to separated templates

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -1,4 +1,33 @@
-@include('layouts.header')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <div class="wrap">
+        <header class="head">
+            <a href="#" class="logo"></a>
+
+            <nav class="main-nav">
+                <ul class="main-nav-list">
+                    <li class="main-nav-item">
+                        <a href="/notas" class="main-nav-link">
+                            <i class="icon icon-th-list"></i>
+                            <span>Ver notas</span>
+                        </a>
+                    </li>
+                    <li class="main-nav-item active">
+                        <a href="/notas/crear" class="main-nav-link">
+                            <i class="icon icon-pen"></i>
+                            <span>Nueva nota</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
         <main class="content">
             <div class="cards">
                 <div class="card card-center">
@@ -18,4 +47,17 @@
                 </div>
             </div>
         </main>
-        @include('layouts.footer')
+        <footer class="foot">
+            <div class="ad">
+                <p>
+                    Esta aplicación es desarrollada en el curso
+                    <a href="https://styde.net/curso-de-laravel-10">Curso de Laravel 10 desde cero</a>.
+                </p>
+            </div>
+            <div class="license">
+                <p>© 2019 Derechos Reservados - Styde Limited</p>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
