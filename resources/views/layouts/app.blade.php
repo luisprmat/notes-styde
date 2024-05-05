6b4f14dd4bf73303ac5a55683a9c1b6ebb6d9104
Luis Parrado <luisprmat@gmail.com>
2024-05-05 00:47:19 -0500

    Using Vite to compile assets - Drop static designs - use Sass

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>{{ $title ?? 'App de Notas' }}</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    @vite(['resources/scss/app.scss'])
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
