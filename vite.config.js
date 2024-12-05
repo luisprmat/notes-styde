031ab3ff6ce714e34b3f9c55cd22276a3660c589
Luis Parrado <luisprmat@gmail.com>
2024-12-05 09:12:32 -0500

    Migrate Sass to avoid deprecations warnings - update Vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler',
+            }
+        }
+    },
     plugins: [
         laravel({
             input: ['resources/scss/app.scss'],
